3f954bf2803159533ce4703cd503154d03de1bfe
armiiinsaber <armin.sm2@gmail.com>
2025-06-21 19:22:18 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,16 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // 'font-sans' now = Inter, 'font-serif' = Playfair Display
+        sans: ["Inter", "sans-serif"],
+        serif: ["Playfair Display", "serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
